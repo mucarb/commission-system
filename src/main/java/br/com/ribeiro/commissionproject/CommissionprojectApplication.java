b30a142ebad1a1cefbcbb56b65f5b7ca1eb2cceb
mucarb <murilo.rb60@gmail.com>
2023-05-22 14:57:25 -0300

    First commit

---
+++ src/main/java/br/com/ribeiro/commissionproject/CommissionprojectApplication.java
@@ -0,0 +1,13 @@
+package br.com.ribeiro.commissionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommissionprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommissionprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ribeiro/commissionproject/CommissionprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ribeiro.commissionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommissionprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
